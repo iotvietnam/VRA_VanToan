9450028bd2052785e29c5532e1a8251f66f95b8b
iotvietnam <maivantoan.cdt@gmail.com>
2017-09-26 21:12:50 +0700

    Bài tập 02
    Bài tập recognition - Lecture 02

---
+++ Lecture 02/Recognition001_Digits.m
@@ -0,0 +1,12 @@
+function Recognition001_Digits()
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+fprintf('\n Load du lieu train');
+imgTrainAll = loadMNISTImages('./train-images.idx3-ubyte');
+lblTrainAll = loadMNISTLabels('./train-labels.idx1-ubyte');
+fprintf('\n Load du lieu test');
+imgTestAll = loadMNISTImages('./t10k-images.idx3-ubyte');
+lblTestAll = loadMNISTLabels('./train-labels.idx1-ubyte');
+fprintf('\n Ket thuc.\n');
+end
+
---
+++ Lecture 02/Recognition002_Digits.m
@@ -0,0 +1,26 @@
+function Recognition002_Digits()
+    fprintf('\n Load du lieu train');
+    imgTrainAll = loadMNISTImages('./train-images.idx3-ubyte');
+    lblTrainAll = loadMNISTLabels('./train-labels.idx1-ubyte');
+
+    fprintf('\n Load du lieu test');
+    imgTestAll = loadMNISTImages('./t10k-images.idx3-ubyte');
+    lblTestAll = loadMNISTLabels('./train-labels.idx1-ubyte');
+
+    nTrainImages = size(imgTrainAll,2);
+    nTrainLabels = size(lblTrainAll,1);
+
+    nTestImages = size(imgTestAll,2);
+    nTestLabels = size(lblTestAll,1);
+
+    nSizeofImage = size(imgTrainAll,1);
+
+    fprintf('\n So luong anh Train: [%d].', nTrainImages);
+    fprintf('\n So luong nhan anh Train: [%d].', nTrainLabels);
+    fprintf('\n So luong anh Test: [%d].', nTestImages);
+    fprintf('\n So luong nhan anh Test: [%d].', nTestLabels);
+    fprintf('\n Kich thuoc cua mot anh: [%d].', nSizeofImage);
+
+
+    fprintf('\n Ket thuc.\n');
+end
